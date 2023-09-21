044c316428653010b93b60521acf75d4827fc363
sabrek15 <reddy.ksb007@gmail.com>
2023-09-21 15:30:36 +0530

    Removed the predefined parameter from reverseStrings.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let word_reverse = "";
+    let n = word.length;
+    for(let i=n-1; i>=0; i--){
+        word_reverse += word[i];
+    }
+    return word_reverse;
 };
 
 // Do not edit below this line
