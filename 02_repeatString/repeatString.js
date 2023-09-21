65e1e317edd738f6dc3990ae664ed780ba6c8e97
sabrek15 <reddy.ksb007@gmail.com>
2023-09-21 15:21:26 +0530

    Written the function for the repeating of strings.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if(times<0) return "ERROR";
+    let ans = "";
+    for(let i=0; i<times; i++){
+        ans += word;
+    }
+    return ans;
 };
 
 // Do not edit below this line
