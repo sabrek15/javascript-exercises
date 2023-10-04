353d06af019a92cbcf77f63c7e06a4869a04a9b5
sabrek15 <reddy.ksb007@gmail.com>
2023-10-05 00:13:47 +0530

    implemented algo to find the oldest person in findTheOldest.js file and removed .skip in the findTheOldest.spec.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentperson) => {
+        const oldestage = getAge(oldest.yearOfBirth,oldest.yearOfDeath);
+        const currentage = getAge(currentperson.yearOfBirth,currentperson.yearOfDeath);
+        return oldestage < currentage ? currentperson : oldest;
+    });
 };
+
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
