e4c487b41da6895441bd1d2f5d53c1df91798e81
sabrek15 <reddy.ksb007@gmail.com>
2023-10-04 14:44:40 +0530

    impleted the fibonacci series in fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fib = [];
+    fib.length = n+1;
+    fib[0] = 0;
+    fib[1] = 1;
+    for(let i=2; i<=n; i++){
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+    return fib[n];
 };
 
 // Do not edit below this line
