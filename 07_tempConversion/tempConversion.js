1f26ba70ac2763dc07383329d1ffff3d65fcfb60
sabrek15 <reddy.ksb007@gmail.com>
2023-10-04 12:20:08 +0530

    Written the mathematical expression for conversion.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let cel = (f-32)*(5/9);
+  let round = Math.round(cel * 10) / 10;
+  return round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fah = c*(9/5) + 32;
+  let rounded = Math.round(fah * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
