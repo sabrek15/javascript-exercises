848c069568c1cacb4b0fe7fcc8167e35ccda8b3c
sabrek15 <reddy.ksb007@gmail.com>
2023-10-04 08:56:10 +0530

    Given the algo to run the problem for all test cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,10 @@
 const sumAll = function(a, b) {
-    if(!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR!";
-    if(a<0||b<0) return "ERROR!";
+    if(!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR";
+    if(a<0||b<0) return "ERROR";
     if(a>b){
-        swap(a,b);
+        let temp = a + b;
+        a = temp - a;
+        b = temp - a;
     }
     let sum =0;
     for(let i=a; i<=b; i++)
