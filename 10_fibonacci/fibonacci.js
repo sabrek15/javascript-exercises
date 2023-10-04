4a070ad95edd9297196fcab9d254448c55c6af79
sabrek15 <reddy.ksb007@gmail.com>
2023-10-04 14:50:49 +0530

    Implemented the fibonacci series in fibonacci.js and removed the .skip in th fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,16 @@
 const fibonacci = function(n) {
-    let fib = [];
-    fib.length = n+1;
-    fib[0] = 0;
-    fib[1] = 1;
-    for(let i=2; i<=n; i++){
-        fib[i] = fib[i-1] + fib[i-2];
+    if(n>=0){
+        let fib = [];
+        fib.length = n+1;
+        fib[0] = 0;
+        fib[1] = 1;
+        for(let i=2; i<=n; i++){
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[n];
+    } else {
+        return "OOPS";
     }
-    return fib[n];
 };
 
 // Do not edit below this line
