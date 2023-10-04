d1336cfc050a0f8423e7c07b1c0b15b4da023f44
sabrek15 <reddy.ksb007@gmail.com>
2023-10-04 08:22:08 +0530

    changed the algo tho the problem.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,12 @@
 const sumAll = function(a, b) {
-    let sum = 0;
-    if(a.isnum()&&b.isnum()){
-        for(let i=a; i<=b; i++){
-            sum += i;
-        }
-        return sum;
-    } else {
-        return "ERROR!";
+    if(!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR!";
+    if(a<0||b<0) return "ERROR!";
+    if(a>b){
+        swap(a,b);
     }
+    let sum =0;
+    for(let i=a; i<=b; i++)
+        sum += i;
     return sum;
 };
 
